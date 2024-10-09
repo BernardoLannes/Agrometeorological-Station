31a0df1df5bc414483517d8f4179169df6476290
BernardoLannes <bernardolannes2003@gmail.com>
2024-10-09 13:28:13 -0300

    adding rain sensor code on arduino

--- arduino-code/arduino-code.ino
+++ arduino-code/arduino-code.ino
@@ -2,10 +2,11 @@
 #include <DHT_U.h>  // Para sensor de temperatura do ar e umidade
 
 // Definição dos Pinos:
-const int ldrPin = A0; // Pino onde o LDR está conectado
-const int umidPin = A1; // Pino onde o Sensor de Umidade do solo está conectado
-const int div_tens = 2; // Pino que fornecerá 5v para as medições apenas quando necessário
-const int dhtPin = 3;   // Pino onde o sensor de temperatura e umidade do ar está conectado
+const int ldrPin = A0;   // Pino onde o LDR está conectado
+const int umidPin = A1;  // Pino onde o Sensor de Umidade do solo está conectado
+const int div_tens = 2;  // Pino que fornecerá 5v para as medições apenas quando necessário
+const int dhtPin = 3;    // Pino onde o sensor de temperatura e umidade do ar está conectado
+const int chuvaPin = A2; // Pino onde o sensor de chuva está conectado
 
 DHT dht(dhtPin, DHT11);
 
@@ -30,13 +31,15 @@ void loop() {
   delay(delay_sensores); // Evita qualquer demora de incialização
 
   // Leitura dos Dados:
-  int ldrValue = analogRead(ldrPin); // Lê o valor do LDR
-  int umidValue = analogRead(umidPin); // Lê o valor da umidade do solo
-  float dhtTemp = dht.readTemperature(); // Lê a temperatura
-  float dhtUmid = dht.readHumidity(); // Lê umidade do ar
+  int ldrValue = analogRead(ldrPin);
+  int umidValue = analogRead(umidPin);
+  int dhtTemp = dht.readTemperature();
+  int dhtUmid = dht.readHumidity();
+  int chuvaValue = analogRead(chuvaPin);
   
   // Interpretação dos Dados:
   umidValue = map(umidValue, 0, 1023, 1023, 0); // Inverte o lógico do sensor de umidade
+  chuvaValue = map(chuvaValue, 0, 1023, 1023, 0); // Inverte o lógico do sensor de chuva
   
   // Envio dos Dados pela Serial:
   Serial.print(ldrValue);
@@ -45,7 +48,9 @@ void loop() {
   Serial.print(", ");
   Serial.print(dhtTemp);
   Serial.print(", ");
-  Serial.println(dhtUmid);
+  Serial.print(dhtUmid);
+  Serial.print(", ");
+  Serial.println(chuvaValue);
 
   // Finalização do Ciclo
   digitalWrite(div_tens, LOW); // Desliga os sensores e divisor de tensão
